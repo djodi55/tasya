ae0e42cc1eb7b5992a59ebb3fb9827060ceaa335
djodi55 <djodizzz@gmail.com>
2020-05-05 02:59:58 +0700

    Upload di Github

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Nanaa", "Babe", "Sayang", "Nanaakuu", "Nananana"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
